60fec2e06a985b3bd88a814fc916e367d1e341fd
= <=>
2023-04-18 16:16:14 -0600

    users can log out

--- controllers/auth.js
+++ controllers/auth.js
@@ -22,4 +22,10 @@ module.exports = (app) => {
       return res.status(400).send({ err });
     }
   });
+
+  // LOGOUT
+  app.get('/logout', (req, res) => {
+    res.clearCookie('nToken');
+    return res.redirect('/');
+  });
 };
